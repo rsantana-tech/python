43756ce1bba7d6d80d8ee6487e1a62b02ab852a6
rsantana-tech <rogeriosantana000@gmail.com>
2025-11-03 23:31:41 -0300

    Add exercise 47: display even numbers from 1 to 50 using for loop

---
+++ guanabara/world2/loop-for/challenge47.py
@@ -0,0 +1,6 @@
+# Python Exercise 47: Create a program that displays on the
+# screen all the even numbers in the range between 1 and 50.
+
+for c in range(1, 50):
+    if c % 2 == 0:
+        print(c)
